3cefa558c7d265cfdb33a9cf9b3778f0d851ce59
Gray <helloworld.graciela@gmail.com>
2025-03-30 20:58:52 -0400

    added comments to player movement code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,17 +19,24 @@ public class PlayerController : MonoBehaviour
     {
         if (!isMoving) 
         { 
+            // gets current input
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
             // removes diagonal movement
             if (input.x != 0) input.y = 0;
             
+            // if there is input
             if (input != Vector2.zero)
             {
+                // tells the animator which way the player is moving
                 animator.SetFloat("MoveX", input.x);
                 animator.SetFloat("MoveY", input.y);
+
+                // sets target position to players current positon
                 var targetPos = transform.position;
+
+                // sets target position to one tile up right left or down from current position
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
@@ -42,8 +49,11 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("IsMoving", isMoving);
         
     }
+
+    // this is what actually moves the player, it takes a target position and smoothly moves the player to that point
     IEnumerator Move(Vector3 targetPos)
     {
+        // player is currently moving between two tiles
         isMoving = true;
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) // if target position and transform position are different enough
         { 
@@ -54,6 +64,7 @@ public class PlayerController : MonoBehaviour
         }
         // if theyre close enough just snap player pos to target pos
         transform.position = targetPos;
+        // player is no longer moving
         isMoving = false;
     }
 }

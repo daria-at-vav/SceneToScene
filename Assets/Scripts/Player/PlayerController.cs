872702bc63d176444037366402958628af721251
Gray <helloworld.graciela@gmail.com>
2025-04-06 23:44:05 -0400

    Removed ObjectTilemap field from playercontroller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     // tilemap that things that cannot be walked on/under are placed on
     public Tilemap solidTilemap;
-    public Tilemap objectTilemap;
     // offset because the rounding in isMovable tile messes up the y position
     public Vector3Int add;
 

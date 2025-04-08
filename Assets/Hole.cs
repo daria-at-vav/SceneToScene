0d0fbb19b18472c41bf4cc8daac3a457fd74ea1b
Gray <helloworld.graciela@gmail.com>
2025-04-08 14:32:09 -0400

    made hole change scene

--- Assets/Hole.cs
+++ Assets/Hole.cs
@@ -3,22 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Hole : NonPlayerObject
 {
-    [SerializeField] Transform playerTransform;
+
     [SerializeField] string nextSceneName;
 
-    private SceneManager sceneChanger;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void Interact()
     {
-        
+        SceneManager.LoadScene(nextSceneName);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if((transform.position - playerTransform.position).sqrMagnitude < Mathf.Epsilon){
-                SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
-        }
-    }
+
 }
